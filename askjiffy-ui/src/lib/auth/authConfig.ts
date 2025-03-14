439a2a38f2dbe124885528cd6a8b2cafb14dccf3
Aansh Kotian <76598799+Aone52@users.noreply.github.com>
2025-03-14 18:00:14 -0400

    Added access token attribute to session, TODO extend user interface to include attributes returned from third party backend + decide between fetching the user profile in the jwt callback or creating a separate hook

--- askjiffy-ui/src/lib/auth/authConfig.ts
+++ askjiffy-ui/src/lib/auth/authConfig.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import 'next-auth/jwt';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [Google],
@@ -10,14 +11,30 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                 token.accessToken = account.access_token
             }
             console.log(token);
+            //add call to fetch user profile here? or just create a separate hook that takes the session.access token and gets the user profile
             return token;
         },
         async session({session, token}){
-            // if (token?.accessToken) session.accessToken = token.accessToken
-
+            //make access token available in the session
+            if(token?.accessToken) session.accessToken = token.accessToken
             return session;
         }
     }
-    //declare module to extend session interface
+   
+
 })
+
+ //declare module to extend session interface
+declare module "next-auth"{
+    interface Session{
+        accessToken?:string
+    }
+}
+
+// https://github.com/nextauthjs/next-auth/issues/9571
+declare module "next-auth/jwt"{
+    interface JWT {
+        accessToken?: string
+    }
+}
 
