aa3ec62befb9d33e4e422c1b4023b001efe6e18b
Aansh Kotian <76598799+Aone52@users.noreply.github.com>
2025-03-14 12:08:24 -0400

    added jwt and session callbacks: NEED to extend session interface to include some new values in token

--- askjiffy-ui/src/lib/auth/authConfig.ts
+++ askjiffy-ui/src/lib/auth/authConfig.ts
@@ -3,5 +3,21 @@ import Google from "next-auth/providers/google"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [Google],
+    session: {strategy : "jwt"},
+    callbacks: {
+        jwt({token, account, profile,trigger}){
+            if(account){
+                token.accessToken = account.access_token
+            }
+            console.log(token);
+            return token;
+        },
+        async session({session, token}){
+            // if (token?.accessToken) session.accessToken = token.accessToken
+
+            return session;
+        }
+    }
+    //declare module to extend session interface
 })
 
